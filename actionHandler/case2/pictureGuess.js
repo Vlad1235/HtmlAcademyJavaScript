a74a8cbaf722731493150a9638def60021730ca6
Vlad1235 <fn1235@bk.ru>
2020-04-10 01:00:33 +0900

    setTimeout function - restoring previous state

--- actionHandler/case2/pictureGuess.js
+++ actionHandler/case2/pictureGuess.js
@@ -10,5 +10,15 @@ function showAnswer(eventObj) {
     var image = eventObj.target;
     var name = image.id;
     name = name + ".jpg"; // так как нормальная фотография называется также как id, но без разрширения .jpg
-    image.src=name;
+    image.src = name;
+    window.setTimeout(reblur, 2000, image); // таймер 2 сек, который запустит обработчик события
+}
+
+/*
+    обработчик, чтобы снова восстановить размытое изображение
+*/
+function reblur(image) {
+    var name = image.id;
+    name = name + "blur.jpg";
+    image.src = name;
 }
