fda0b40ab16b62a7733b03b602487158227a93fb
Vlad1235 <fn1235@bk.ru>
2020-05-27 22:00:56 +0900

    speacial JavaScript boolean logic

---
+++ JavaScriptES6/leacture5/js/script.js
@@ -0,0 +1,81 @@
+"use strict";
+
+
+// // to string
+// console.log(typeof (String(null))); // string 
+// console.log(typeof (String(4))); // string 
+
+
+// console.log(typeof (5 + " "));
+
+// const num = 5;
+// console.log("https://vk.com/catalog/" + num);
+
+// const fontSize = 26 + "px";
+
+
+
+// console.log(typeof (Number("4"))); // number
+
+// console.log(typeof (+"5")); // number
+
+// console.log(typeof (parseInt("15px", 10)));
+
+// let answer = +prompt("Hello", "");
+
+
+
+// let switcher = 1;
+// if (switcher) {
+//     console.log("working");
+// }
+
+// console.log(typeof (Boolean("4"))); // boolean
+
+// console.log(typeof (!!"4444"));
+
+
+
+
+// [] + false - null + true
+// console.log(typeof ([] + false)); // string
+
+// console.log([ ] + false - null + true);
+
+
+// let y = 1; 
+// let x = y = 2; 
+// alert(x);
+
+
+// console.log([ ] + 1 + 2); // "12"
+
+// alert( "1"[0] );
+
+// console.log(2 && 1 && null && 0 && undefined);
+
+// console.log(!!(1 && 2) === (1 && 2));
+
+
+// console.log(typeof ((1 && 2))); // number
+// console.log(1 && 2); // 2 
+
+// console.log(typeof (!!(1 && 2))); //boolean
+// console.log(!!(1 && 2)); // true
+
+
+
+// alert( null || 2 && 3 || 4 );
+
+
+// const a = [1,2,3];
+// const b = [1,2,3];
+// console.log(a===b); // false
+
+
+// alert( +"5"/2 );
+
+// console.log("Ёжик" > "яблоко"); // false
+
+
+console.log(0 || "" || 2 || undefined || true || falsе); // 2
