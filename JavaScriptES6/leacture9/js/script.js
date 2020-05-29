c2124f94f783a2d5655a0fdb96723d833f7837a5
Vlad1235 <fn1235@bk.ru>
2020-05-29 14:10:36 +0900

    mobile listenerHandlers

---
+++ JavaScriptES6/leacture9/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const box = document.querySelector(".box");
+    box.addEventListener("touchstart",(e)=>{
+        e.preventDefault();
+        console.log("start");
+    });
+
+    
+    box.addEventListener("touchmove",(e)=>{
+        e.preventDefault();
+        console.log("move");
+    });
+
+    box.addEventListener("touchend",(e)=>{
+        e.preventDefault();
+        console.log("move");
+    });
+
+
+});
