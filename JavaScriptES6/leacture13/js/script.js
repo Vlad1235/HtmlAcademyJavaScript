583addae0763e0abbf2b8534b629a31d4534d5ea
Vlad1235 <fn1235@bk.ru>
2020-05-31 20:26:18 +0300

    work with dates in JS

---
+++ JavaScriptES6/leacture13/js/script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+// const now = new Date("2020-05-30");
+// console.log(now); // 2020-05-30T00:00:00.000Z
+
+
+
+// const now1 = new Date(2020, 5, 1, 20);
+// console.log(now1);
+
+
+// const now2 = new Date(0);
+// console.log(now2);
+
+
+// const now3 = new Date(-999999999999);
+// console.log(now3);
+
+// const now4 = new Date();
+// console.log(now4.getFullYear());
+// console.log(now4.getMonth());
+// console.log(now4.getDate()); 
+// console.log(now4.getHours());
+// console.log(now4.getMinutes());
+// console.log(now4.getSeconds());
+// console.log(now4.getMilliseconds());
+// console.log(now4.getDay());
+
+// console.log(now4.getHours());
+// console.log(now4.getUTCHours());
+
+// // console.log(now4.getTimezoneOffset());
+// console.log(now4.getTime());
+
+// console.log(now4.getDate(1590820237679));
+// console.log(now4.getFullYear(1590820237679));
+// console.log(now4.getMonth(1590820237679));
+
+// const now5 = new Date((1590820237679));
+// console.log(now5); // 2020-05-30T06:30:37.679Z
+
+// const now = new Date();
+// const now2 = new Date();
+// console.log(now); 
+// now2.setHours(84);
+// console.log(now2);
+
+
+// const now = new Date("2020-05-01");
+// console.log(now); 
+// new Date.parse("2020-05-01");
+
+// let start = new Date();
+// for (let i = 0; i < 100000; i++) {
+//     let some = i ** 3;
+// }
+
+// let end = new Date();
+
+// alert(`Цикл отработал за ${end-start} миллисекунд`);
